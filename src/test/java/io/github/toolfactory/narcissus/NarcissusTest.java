8a111a99069d67e144c863fd17db45f25a6854a2
Luke Hutchison <luke.hutch@gmail.com>
2021-09-22 12:23:34 -0600

    Update package for tests

--- src/test/java/io/github/toolfactory/narcissus/NarcissusTest.java
+++ src/test/java/io/github/toolfactory/narcissus/NarcissusTest.java
@@ -1,4 +1,4 @@
-package narcissus;
+package io.github.toolfactory.narcissus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
