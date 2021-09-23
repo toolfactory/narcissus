2d804ea2a216584a68633c11ff85759f619f4146
Luke Hutchison <luke.hutch@gmail.com>
2021-09-23 13:03:46 -0600

    Make tests work on JDK 7

--- src/test/java/io/github/toolfactory/narcissus/NarcissusTest.java
+++ src/test/java/io/github/toolfactory/narcissus/NarcissusTest.java
@@ -1,7 +1,6 @@
 package io.github.toolfactory.narcissus;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -9,8 +8,7 @@ import java.lang.reflect.Modifier;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
+import org.junit.Test;
 
 public class NarcissusTest {
     static class X {
@@ -154,40 +152,28 @@ public class NarcissusTest {
         }
     }
 
-    @Test
-    public void testCheckNullPointerException() throws Exception {
+    @Test(expected = NullPointerException.class)
+    public void testCheckNullPointerExceptionNonStatic() throws Exception {
         Method dm = Narcissus.findMethod(Z.class, "d");
-        assertThrows(NullPointerException.class, new Executable() {
-        	public void execute() {
-        		Narcissus.invokeDoubleMethod(null, dm);
-        	}
-        });
-        Method _dm = Narcissus.findMethod(Z.class, "_d");
-        assertThrows(NullPointerException.class, new Executable() {
-        	public void execute() {
-        		Narcissus.invokeDoubleMethod(null, _dm);
-        	}
-        });
+        Narcissus.invokeDoubleMethod(null, dm);
     }
 
-    @Test
+    @Test(expected = NullPointerException.class)
+    public void testCheckNullPointerExceptionStatic() throws Exception {
+        Method _dm = Narcissus.findMethod(Z.class, "_d");
+        Narcissus.invokeDoubleMethod(null, _dm);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
     public void testCheckStaticModifierException1() throws Exception {
         Method dm = Narcissus.findMethod(Z.class, "d");
-        assertThrows(IllegalArgumentException.class, new Executable() {
-        	public void execute() {
-        		Narcissus.invokeStaticDoubleMethod(dm);
-        	}
-        });
+        Narcissus.invokeStaticDoubleMethod(dm);
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void testCheckObjectClassDoesNotMatchDeclaringClass() throws Exception {
         Method dm = Narcissus.findMethod(Z.class, "d");
-        assertThrows(IllegalArgumentException.class, new Executable() {
-        	public void execute() {
-        		Narcissus.invokeDoubleMethod(new Y(), dm);
-        	}
-        });
+        Narcissus.invokeDoubleMethod(new Y(), dm);
     }
 
     @Test
@@ -202,24 +188,23 @@ public class NarcissusTest {
 
     @Test
     public void testEnumerateFields() throws Exception {
-        assertThat(Narcissus.enumerateFields(Y.class).stream().map(new Function<Field, String> () {
-        	@Override
-        	public String apply(Field field) {
-        		return field.getName();
-        	}
-        }).collect(Collectors.toList()))
-                .containsOnly("i", "j", "s", "c", "b", "z", "f", "d", "_i", "_j", "_s", "_c", "_b", "_z", "_f",
-                        "_d");
+        assertThat(Narcissus.enumerateFields(Y.class).stream().map(new Function<Field, String>() {
+            @Override
+            public String apply(Field field) {
+                return field.getName();
+            }
+        }).collect(Collectors.toList())).containsOnly("i", "j", "s", "c", "b", "z", "f", "d", "_i", "_j", "_s",
+                "_c", "_b", "_z", "_f", "_d");
     }
 
     @Test
     public void testEnumerateMethods() throws Exception {
-        assertThat(Narcissus.enumerateMethods(Z.class).stream().map(new Function<Method, String> () {
-        	@Override
-        	public String apply(Method method) {
-        		return method.getName();
-        	}
-        }).collect(Collectors.toList()))
-                .contains("i", "j", "s", "c", "b", "z", "f", "d", "_i", "_j", "_s", "_c", "_b", "_z", "_f", "_d");
+        assertThat(Narcissus.enumerateMethods(Z.class).stream().map(new Function<Method, String>() {
+            @Override
+            public String apply(Method method) {
+                return method.getName();
+            }
+        }).collect(Collectors.toList())).contains("i", "j", "s", "c", "b", "z", "f", "d", "_i", "_j", "_s", "_c",
+                "_b", "_z", "_f", "_d");
     }
 }
